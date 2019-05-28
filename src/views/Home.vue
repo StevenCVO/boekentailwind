f62be4f7953954209df6109a58a648c720efe321
unknown <scripter@BK102PC25.sjca.lan>
2019-05-28 22:11:18 +0200

    Modal toegevoegd en CRUD afgewerkt

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -19,30 +19,95 @@
           Nieuw boek
         </button>
       </template>
-      <template v-slot:acties>
+      <template v-slot:acties="{ item }">
         <td>
-          <i @click="wijzigBoek" class="material-icons cursor-pointer">edit</i>
-          <i @click="verwijderBoek" class="material-icons cursor-pointer"
+          <i @click="wijzigBoek(item)" class="material-icons cursor-pointer"
+            >edit</i
+          >
+          <i @click="verwijderBoek(item)" class="material-icons cursor-pointer"
             >delete</i
           >
         </td>
       </template>
     </data-tabel>
+
+    <modal @sluiten="toonModal = false" v-show="toonModal">
+      <h1 class="text-2xl text-center mb-6">
+        Boek {{ wijzigen ? "wijzigen" : "toevoegen" }}
+      </h1>
+      <div class="flex">
+        <label class="w-48" for="titel">Titel:</label>
+        <input
+          class="flex-grow border-b border-gray-200 mb-6 outline-none"
+          type="text"
+          id="titel"
+          v-model="boek.titel"
+        />
+      </div>
+      <div class="flex">
+        <label class="w-48" for="aantalPaginas">Aantal Pagina's:</label>
+        <input
+          class="flex-grow border-b border-gray-200 mb-6 outline-none"
+          id="aantalPaginas"
+          type="text"
+          v-model.number="boek.aantalPaginas"
+        />
+      </div>
+      <div class="flex">
+        <label class="w-48" for="auteur">Auteur:</label>
+        <select
+          class="flex-grow border-b border-gray-200 mb-6 outline-none"
+          v-model="boek.auteur"
+        >
+          <option
+            v-for="auteur of auteurs"
+            :key="auteur._id"
+            :value="auteur._id"
+          >
+            {{ auteur.naam }}
+          </option>
+        </select>
+      </div>
+      <div class="flex justify-around">
+        <button
+          class="rounded border border-1 border-gray-400 px-4 py-2
+                 text-black w-32"
+          @click="toonModal = false"
+        >
+          Annuleren
+        </button>
+        <button
+          class="rounded bg-blue-400 px-4 py-2
+                 text-white w-32"
+          @click="postData"
+        >
+          {{ wijzigen ? "Wijzigen" : "Toevoegen" }}
+        </button>
+      </div>
+    </modal>
   </div>
 </template>
 
 <script>
 import axios from "axios";
 import DataTabel from "@/components/DataTabel.vue";
+import Modal from "@/components/Modal.vue";
 
 export default {
   name: "home",
   components: {
-    DataTabel
+    DataTabel,
+    Modal
   },
   data() {
     return {
+      boek: {
+        titel: "",
+        aantalPaginas: 1,
+        auteur: ""
+      },
       boeken: [],
+      auteurs: [],
       kolommen: [
         {
           naam: "Titel",
@@ -62,24 +127,71 @@ export default {
           sorteerbaar: true,
           zoekbaar: false
         }
-      ]
+      ],
+      toonModal: false,
+      wijzigen: false
     };
   },
   methods: {
     nieuwBoek() {
-      // Nieuw boek toevoegen
+      this.wijzigen = false;
+      this.boek = {
+        titel: "",
+        aantalPaginas: 1,
+        auteur: ""
+      };
+      this.toonModal = true;
     },
-    wijzigBoek() {
-      // Boek wijzigen
+    wijzigBoek(boek) {
+      this.wijzigen = true;
+      this.boek = {
+        ...boek,
+        auteur: boek.auteur._id
+      };
+      this.toonModal = true;
     },
-    verwijderBoek() {
+    async postData() {
+      if (this.wijzigen) {
+        // Wijzigen van boek in databank
+        const gewijzigdBoek = await axios.put(
+          "http://localhost:7000/boeken/" + this.boek._id,
+          this.boek
+        );
+
+        const index = this.boeken.findIndex(boek => {
+          return boek._id === this.boek._id;
+        });
+
+        this.boeken.splice(index, 1, gewijzigdBoek.data);
+        this.toonModal = false;
+      } else {
+        // Toevoegen van boek in databank
+        const nieuwBoek = await axios.post(
+          "http://localhost:7000/boeken",
+          this.boek
+        );
+
+        this.boeken.push(nieuwBoek.data);
+        this.toonModal = false;
+      }
+    },
+    async verwijderBoek(boek) {
       // Boek verwijderen
+      try {
+        await axios.delete("http://localhost:7000/boeken/" + boek._id);
+        this.boeken.splice(this.boeken.indexOf(boek), 1);
+      } catch (err) {
+        console.log(err);
+      }
     }
   },
   async created() {
     // Data ophalen van server
     const boeken = await axios("http://localhost:7000/boeken");
     this.boeken = boeken.data;
+
+    const auteurs = await axios("http://localhost:7000/auteurs");
+    this.auteurs = auteurs.data;
   }
 };
 </script>
